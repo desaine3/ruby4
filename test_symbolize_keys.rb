602daf59f5f7dd99f7390e2f76727253401cd243
Tom Close <tom.close@cantab.net>
2013-06-27 13:28:14 +0100

    Test for symbolize keys

---
+++ test_symbolize_keys.rb
@@ -0,0 +1,14 @@
+# Load in standard test frameworks
+require 'minitest/spec'
+require 'minitest/autorun'
+
+# load in symbolize_keys.rb
+require_relative 'symbolize_keys'
+
+
+# do the test
+describe 'symbolize_keys' do
+	it "handles an array with mixes keys" do
+		assert_equal {:a=>1, :b=>2}, symbolize_keys({'a'=>1, :b=>2})
+	end
+end
