d5672e98c5ba676020831edbb64ae16f7c9f10f2
Tom Close <tom.close@cantab.net>
2013-07-13 22:01:23 +0100

    Fixed test

--- test_i_have_a_dream.rb
+++ test_i_have_a_dream.rb
@@ -8,19 +8,19 @@ require_relative 'i_have_a_dream'
 
 # do the test
 describe 'frequencies' do
-	it "counts elements in an array" do
-		assert_equal({'a'=>1, 'b'=>2}, frequencies(['a b a']))
-	end
+    it "counts elements in an array" do
+        assert_equal({'a'=>1, 'b'=>2}, frequencies(['a b a']))
+    end
 end
 
 describe 'dream_frequency' do
-	it "works for 50 " do
-		output = <<EOF
+    it "works for 50 " do
+        output = <<EOF
 to: 50
 the: 103
 of: 99
 and: 54
 EOF
-		assert_output(output) { dream_frequency(50)}
-	end
+        assert_output(output) { dream_frequency(50)}
+    end
 end
