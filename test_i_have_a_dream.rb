4863a180e06963e2e6e805fdbf8e6c103a14b6bf
Peter Inglesby <peter.inglesby@gmail.com>
2013-07-18 13:34:52 +0100

    Fix tests

--- test_i_have_a_dream.rb
+++ test_i_have_a_dream.rb
@@ -9,14 +9,12 @@ require_relative 'i_have_a_dream'
 # do the test
 describe 'frequencies' do
     it "counts elements in an array" do
-        assert_equal({'a'=>1, 'b'=>2}, frequencies('a b a'))
+        assert_equal({'a'=>2, 'b'=>1}, frequencies('a b a'))
     end
 end
 
-describe 'remove_stopwords' do
-    it "removes stopwords from the hash" do
-
-        assert_equal({"cat" => 1, "dog" => 1},remove_stopwords({"a"=>2, "cat"=> 1, "and"=>1, "dog"=>1}))
+describe 'remove_stop_words' do
+    it "removes stop_words from the hash" do
+        assert_equal({"cat" => 1, "dog" => 1}, remove_stop_words({"a"=>2, "cat"=> 1, "and"=>1, "dog"=>1}))
     end
-
 end
--- test_symbolize_keys.rb
+++ test_symbolize_keys.rb
@@ -9,6 +9,6 @@ require_relative 'symbolize_keys'
 # do the test
 describe 'symbolize_keys' do
     it "handles an array with keys that are strings or symbols" do
-        assert_equal {:a=>1, :b=>2}, symbolize_keys({'a'=>1, :b=>2})
+        assert_equal({:a=>1, :b=>2}, symbolize_keys({'a'=>1, :b=>2}))
     end
 end
