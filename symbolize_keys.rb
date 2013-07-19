71306746d5fdf7fe67fe9836ad132c5f34ca20ad
Elina Desaine <edesaine@ticklo.com>
2013-07-19 12:21:22 +0100

    finished

--- symbolize_keys.rb
+++ symbolize_keys.rb
@@ -5,6 +5,9 @@
 def symbolize_keys(h)
     new_h = {}
     # iterate over h and put the values into new_h
+    h.each do |key, value|
+        new_h[key.to_sym] = value
 
+    end
     return new_h
 end
